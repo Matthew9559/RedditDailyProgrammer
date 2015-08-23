1cd356343435e8f209632b140b1dc7d7d7c45b38
Matt Sonnhalter <matthew9559@hotmail.com>
2015-08-23 16:19:57 -0500

    Added link to Reddit post

--- Solution/Solution/Problems/2015_08_17_Easy.cs
+++ Solution/Solution/Problems/2015_08_17_Easy.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Challenge #228: Letters in Alphabetical Order
+//https://www.reddit.com/r/dailyprogrammer/comments/3h9pde/20150817_challenge_228_easy_letters_in/
+
 namespace Solution
 {
     public class _2015_08_17_Easy : IRunSolution
