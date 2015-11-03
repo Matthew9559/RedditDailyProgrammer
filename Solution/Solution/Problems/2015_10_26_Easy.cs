3c4515b9fe78591b89028b785bb477972b278ffd
Matt Sonnhalter <matthew9559@hotmail.com>
2015-11-03 12:45:51 -0600

    Added new daily challenge and added a unit testing proeject with NUnit

---
+++ Solution/Solution/Problems/2015_10_26_Easy.cs
@@ -0,0 +1,24 @@
+﻿using Solution.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//[2015-10-26] Challenge #238 [Easy] Consonants and Vowels
+//https://www.reddit.com/r/dailyprogrammer/comments/3q9vpn/20151026_challenge_238_easy_consonants_and_vowels/
+
+namespace Solution.Problems
+{
+    public class _2015_10_26_Easy : IRunSolution
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+//
