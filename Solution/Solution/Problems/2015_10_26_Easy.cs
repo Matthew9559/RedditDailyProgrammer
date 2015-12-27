b45e7ce434aae0de4dc4a5f2f15221b14594b399
Matt Sonnhalter <matthew9559@gmail.com>
2015-12-26 19:16:24 -0500

    Fixed solution and removed unit test

--- Solution/Solution/Problems/2015_10_26_Easy.cs
+++ Solution/Solution/Problems/2015_10_26_Easy.cs
@@ -1,9 +1,5 @@
 ﻿using Solution.Interface;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 //[2015-10-26] Challenge #238 [Easy] Consonants and Vowels
 //https://www.reddit.com/r/dailyprogrammer/comments/3q9vpn/20151026_challenge_238_easy_consonants_and_vowels/
@@ -14,61 +10,88 @@ namespace Solution.Problems
     {
         public void Execute()
         {
-            const string lowerAlphabetVowels = "aeiou";
-            const string upperAlphabetVowels = "AEIOU";
-            const string lowerAlphabetConstants = "bcdfghjklmnpqrstvwxyz";
-            const string upperAlphabetConstants = "BCDFGHJKLMNPQRSTVWXYZ";
-
             Console.WriteLine("Please enter constants and vowels only");
             var input = Console.ReadLine();
             input.ToCharArray();
-            
-            foreach (var letter in input)
+
+            if (CheckForInvalidCharacters(input) == true)
             {
-                if (CheckIfNotVowel(letter) == true && CheckIfNotConstant(letter) == true)
-                {
-                    Console.WriteLine("Please restart the program and enter only constants and vowels");
-                    return;
-                }
+                Console.WriteLine("Please re-run the program and enter constants or vowels only");
+                Console.ReadLine();
+                return;
             }
 
+            var randomInstance = new Random();
+
+            var result = ConvertIntoConstantsAndVariables(input, randomInstance);
+
+            Console.WriteLine(result);
+        }
+
+        public bool CheckIfVowel(char input)
+        {
+            bool isVowel = "aeiouAEIOU".IndexOf(input) > 0;
+            return isVowel;
+        }
+
+        public bool CheckIfConstant(char input)
+        {
+            bool isConstant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".IndexOf(input) > 0;
+            return isConstant;
+        }
+
+        public bool CheckForInvalidCharacters(string input)
+        {
+            foreach (var letter in input)
+            {
+                if (CheckIfVowel(letter) == false && CheckIfConstant(letter) == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string ConvertIntoConstantsAndVariables(string input, Random randomInstance)
+        {
             string result = "";
+            char[] alphabetVowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            char[] alphabetConstants = new char[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+            var alphabetVowelsCount = alphabetVowels.Length;
+            var alphabetConstantsCount = alphabetConstants.Length;
 
             foreach (var letter in input)
             {
                 bool isUpper = char.IsUpper(letter);
-                if (isUpper)
-                {
-                    result = GetRandomCharacter(letter);
-                }
-                else
-                {
 
+                if (isUpper == true && letter == 'V')
+                {
+                    var nextLetterPosition = randomInstance.Next(alphabetVowelsCount);
+                    var tempValue = alphabetVowels[nextLetterPosition];
+                    char.ToUpper(tempValue);
+                    result = result + tempValue;
+                }
+                else if (isUpper == true && letter == 'C')
+                {
+                    var nextLetterPosition = randomInstance.Next(alphabetConstantsCount);
+                    var tempValue = alphabetConstants[nextLetterPosition];
+                    char.ToUpper(tempValue);
+                    result = result + tempValue;
+                }
+                else if (isUpper == false && letter == 'v')
+                {
+                    var nextLetterPosition = randomInstance.Next(alphabetVowelsCount);
+                    var tempValue = alphabetVowels[nextLetterPosition];
+                    result = result + tempValue;
+                }
+                else if (isUpper == false && letter == 'c')
+                {
+                    var nextLetterPosition = randomInstance.Next(alphabetConstantsCount);
+                    var tempValue = alphabetConstants[nextLetterPosition];
+                    result = result + tempValue;
                 }
             }
-
-            //output results
-
-        }
-
-        public char GetRandomCharacter(string alphabet, char letter)
-        {
-            Random rng = new Random();
-            int lengthOfAlphabet = alphabet.Length;
-            
-            return input;
-        }
-
-        public bool CheckIfNotVowel(char input)
-        {
-            bool isNotVowel = "aeiouAEIOU".IndexOf(input) < 0;
-            return isNotVowel;
-        }
-
-        public bool CheckIfNotConstant(char input)
-        {
-            bool isNotConstant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".IndexOf(input) < 0;
-            return isNotConstant;
+            return result;
         }
     }
 }
